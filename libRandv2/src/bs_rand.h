e7832be3e79cb6ac4d6916e91e7e47964b50906a
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-02-22 10:40:17 +0100

    libRandv2: Add bs_rand.h header
    Add top header for developers convenience
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

---
+++ libRandv2/src/bs_rand.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef BS_RAND_H
+#define BS_RAND_H
+
+#include "bs_rand_inline.h"
+#include "bs_rand_main.h"
+
+#endif /* BS_RAND_H */
